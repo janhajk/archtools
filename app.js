37dad7316e5dc7a9e12050902f98d93aa5229416
janhajk <janhajk@gmail.com>
2015-01-07 07:04:07 +0000

    gui

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-var util  = require('util');
+var util = require('util');
 var spawn = require('child_process').spawn;
 var http = require('http');
+fs = require('fs');
 
 var PORT = 1337;
 var PATHARCHOIND = '/home/pi/ARCH/src/archcoind';
 
-
 http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  ls = spawn(PATHARCHOIND, ['getinfo']);
-  ls.stdout.on('data', function (data) {
-    response.write('<html><head><script type="text/javascript">var wallet = '+data+';</script></head><body></body></html>');
-    response.end();
-  });
-  ls.stderr.on('data', function (data) {
-    response.write('stderr: ' + data);
-    response.end();
-  });
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    ls = spawn(PATHARCHOIND, ['getinfo']);
+    ls.stdout.on('data', function(wallet_data) {
+        fs.readFile('client.js', 'utf8', function(err, js) {
+            if(err) {
+                return console.log(err);
+            }
+            response.write('<html><head><script type="text/javascript">var wallet = ' + wallet_data + ';');
+            response.write(js);
+            response.write('</script></head><body></body></html>');
+        });
+        response.end();
+    });
+    ls.stderr.on('data', function(data) {
+        response.write('stderr: ' + data);
+        response.end();
+    });
 }).listen(PORT);
