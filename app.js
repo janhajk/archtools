86e75652b47196ab18758dcca385dd698bb0ba35
janhajk <janhajk@gmail.com>
2015-01-07 13:45:58 +0000

    gui

--- app.js
+++ app.js
@@ -7,38 +7,37 @@ var url = require('url');
 
 
 http.createServer(function(request, response) {
-        var purl = url.parse(req.url, true);
-        switch(purl.pathname) {
-            case '/wallet':
-                response.writeHead(200, {
-                    'Content-Type': 'text/html'
-                });
-                ls = spawn(config.path_wallet, ['getinfo']);
-                ls.stdout.on('data', function(wallet_data) {
-                    fs.readFile('client.js', 'utf8', function(err, js) {
-                        response.write('<html><head></head><body></body>');
-                        response.write('<script type="text/javascript">var wallet_data = ' + wallet_data + ';' + js + '</script></html>');
-                        response.end();
-                    });
-                });
-                ls.stderr.on('data', function(data) {
-                    response.write('stderr: ' + data);
+    var purl = url.parse(req.url, true);
+    switch(purl.pathname) {
+        case '/wallet':
+            response.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            ls = spawn(config.path_wallet, ['getinfo']);
+            ls.stdout.on('data', function(wallet_data) {
+                fs.readFile('client.js', 'utf8', function(err, js) {
+                    response.write('<html><head></head><body></body>');
+                    response.write('<script type="text/javascript">var wallet_data = ' + wallet_data + ';' + js + '</script></html>');
                     response.end();
                 });
-                break;
-            case '/wallet/status':
-                response.writeHead(200, {
-                    'Content-Type': 'application/json'
-                });
-                ls = spawn(config.path_wallet, ['getinfo']);
-                ls.stdout.on('data', function(wallet_data) {
-                    response.write(wallet_data);
-                    response.end();
-                });
-        }); ls.stderr.on('data', function(data) {
-        response.write('stderr: ' + data);
-        response.end();
-    });
-    break;
-}
+            });
+            ls.stderr.on('data', function(data) {
+                response.write('stderr: ' + data);
+                response.end();
+            });
+            break;
+        case '/wallet/status':
+            response.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
+            ls = spawn(config.path_wallet, ['getinfo']);
+            ls.stdout.on('data', function(data) {
+                response.end(data);
+            });
+            ls.stderr.on('data', function(data) {
+                response.write('stderr: ' + data);
+                response.end();
+            });
+            break;
+    }
 }).listen(config.port);
